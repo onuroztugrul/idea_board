8406784a71eef4289f3fa773a9326ec4c376d529
Onur Öztuğrul <onur.oztugrul@ipos.net>
2016-08-09 12:07:15 +0300

    add index category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808164534) do
+ActiveRecord::Schema.define(version: 20160809081830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160808164534) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "color"
+    t.index ["name"], name: "index_categories_on_name", unique: true, using: :btree
   end
 
   create_table "ideas", force: :cascade do |t|
